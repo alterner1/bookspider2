e2a7e9141a490d08a4e15581ae97b82e998c6f24
pavel.prima <paprima@edu.hse.ru>
2024-12-14 20:36:37 +0300

    Initial upload

---
+++ README.md
@@ -0,0 +1,33 @@
+# bookspider
+Scrapy Tutorial - An open source and collaborative framework for extracting the data you need from websites.
+
+Visit [scrapy.org](https://scrapy.org/) for more information.
+
+# How to install Scrapy (Ubuntu 20.04.5 LTS)
+
+## Create Python virtual environments
+```
+sudo apt install python3.12-venv
+python3 -m venv venv
+source venv/bin/activate
+```
+
+## Install Scrapy
+```
+pip install scrapy
+```
+
+## Create your Scrapy project
+```
+scrapy startproject bookvoed
+```
+## Build and run your first spider with:
+```
+cd bookvoed
+scrapy genspider bookspider
+scrapy crawl bookspider
+```
+
+````
+userpp@compute-vm-2-2-20-hdd-1731860998953:~/bookspider$  scrapy crawl bookspider
+````
---
+++ bookvoed/pipelines.py
@@ -0,0 +1,45 @@
+# Define your item pipelines here
+#
+# Don't forget to add your pipeline to the ITEM_PIPELINES setting
+# See: https://docs.scrapy.org/en/latest/topics/item-pipeline.html
+
+
+# useful for handling different item types with a single interface
+from itemadapter import ItemAdapter
+import psycopg
+import os
+import time
+class BookvoedPipeline:
+    def __init__(self):
+        db_name = os.getenv('db_name')
+        db_user = os.getenv('db_user')
+        db_pswd = os.getenv('db_pswd')
+        db_host = os.getenv('db_host')
+        self.conn = psycopg.connect(database=db_name,user=db_user,password=db_pswd, host=db_host, port=6432)
+        cursor = self.connection.cursor()
+        cursor.execute('select 1')
+        test_result = cursor.fetchall()
+        if test_result != [(1,)]:
+            raise Exception('Error while initilizing DB connection')
+
+        cursor.execute("""
+            CREATE TABLE IF NOT EXISTS items (
+                id serial PRIMARY KEY,
+                name VARCHAR(255) NOT NULL,
+                author VARCHAR(255),
+                price VARCHAR(255)             
+            )
+        """)
+        self.connection.commit()
+
+    def process_item(self, item, spider):
+        time.sleep(1)
+        if 'error' in item:
+            print(f'Error item recieved, skip: {item}')
+            return item
+        
+        cursor = self.connection.cursor()
+        cursor.execute('INSERT INTO items (name, author, price) VALUES (%s, %s, %s)', [item['name'], item['author'], item['price']])
+        self.connection.commit()
+
+        return item
