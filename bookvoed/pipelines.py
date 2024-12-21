a4be8cac7286445723ee0e4b5ccaa2206915832a
pavel.prima <paprima@edu.hse.ru>
2024-12-21 19:52:06 +0300

    update pipelines

--- bookvoed/pipelines.py
+++ bookvoed/pipelines.py
@@ -11,12 +11,7 @@ import os
 import time
 class BookvoedPipeline:
     def __init__(self):
-        # db_name = os.getenv('db_name')
-        # db_user = os.getenv('db_user')
-        # db_passwd = os.getenv('db_passwd')
-        # db_host = os.getenv('db_host')
         self.connection = psycopg2.connect(os.getenv('CONNECTION_STRING'))
-        # self.connection = psycopg2.connect(database='${db_name}',user='${db_user}',password='${db_passwd}', host='${db_host}', port=6432)
         cursor = self.connection.cursor()
         cursor.execute('select 1')
         test_result = cursor.fetchall()
@@ -40,7 +35,7 @@ class BookvoedPipeline:
             return item
         
         cursor = self.connection.cursor()
-        cursor.execute('INSERT INTO book_data (name, author, price) VALUES (%s, %s, %s)', [item['name'], item['author'], item['price']])
+        cursor.execute('INSERT INTO book_data (name, author, price) VALUES (%s, %s, %s)', [item['Название'], item['Автор(ы)'], item['Цена']])
         self.connection.commit()
 
         return item
